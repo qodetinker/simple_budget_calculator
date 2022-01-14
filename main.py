8e9c53c7f31682985685c1968cedbfb619587c81
Qode Tinker <13643000+qodetinker@users.noreply.github.com>
2022-01-14 12:21:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+needs           = 0.50
+wants           = 0.25
+saves           = 0.25
+
+
+print(
+    "This is a simple budget calculator based off the 50/30/20 rule" + "\n"
+    "50 percent Needs" + "\n"
+    "30 percent Wants" + "\n"
+    "20 percent savings" + "\n"
+    )
+
+totalMonthlyNetPay = float(input("Please enter your total monthly pay:"))
+
+print(
+    "\n"
+    "Here are your monthly totals" "\n"
+    f"50 percent Needs {needs*totalMonthlyNetPay}" + "\n"
+    f"30 percent wants {wants*totalMonthlyNetPay}" + "\n"
+    f"20 percent savings {saves*totalMonthlyNetPay}"
+    )
